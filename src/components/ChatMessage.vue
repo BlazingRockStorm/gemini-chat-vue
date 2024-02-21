73c0d3cf719beb59593ec1f3257d80a7da982fd8
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-02-21 18:54:27 +0900

    fix

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -3,12 +3,12 @@
     <v-row class="d-flex overflow-auto">
       <v-col cols="12" sm="2" class="align-start">
         <v-sheet class="ma-2 pa-2">
-          <v-btn :append-icon="message.role === 'user' ? 'mdi-account' : 'mdi-assistant'">{{ message.role }}</v-btn>
+          <v-btn :append-icon="msg.role === 'user' ? 'mdi-account' : 'mdi-assistant'">{{ msg.role }}</v-btn>
         </v-sheet>
       </v-col>
       <v-col cols="12" sm="10" class="align-self-center">
         <v-sheet class="ma-2 pa-2">
-          <pre v-html="message.content" class="preWrap"></pre>
+          <pre v-html="msg.content" class="preWrap"></pre>
         </v-sheet>
       </v-col>
     </v-row>
