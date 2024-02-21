95136187f9539835b26dad8749b609603c136456
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-02-21 15:59:47 +0900

    rename component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChatScreen msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatScreen from './components/ChatScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ChatScreen
   }
 }
 </script>
--- src/components/ChatScreen.vue
+++ src/components/ChatScreen.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'ChatScreen',
   props: {
     msg: String
   }
