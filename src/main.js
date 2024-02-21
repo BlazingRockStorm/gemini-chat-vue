e0196044107a3fc0a7f28cb6f0b0a906d4ea71e8
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-02-21 19:11:46 +0900

    remove comments

--- src/main.js
+++ src/main.js
@@ -7,21 +7,6 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-// import { GoogleGenerativeAI } from '@google/generative-ai'
-
-// // Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(process.env.VUE_APP_GOOGLE_GEMINI_API_KEY);
-
-// // ...
-
-// const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
-// const prompt = "Info about the song The River by Good Charlotte and A7x"
-
-// const result = await model.generateContent(prompt);
-// const response = await result.response;
-// const text = response.text();
-// console.log(text);
 const vuetify = createVuetify({
   components,
   directives,
